2b2dbedc3219abae70c79ad31b7e447906fbd8f9
Joshua Boyd <jaboydfamily@gmail.com>
2021-05-16 12:15:38 -0400

    testing dynamic content transition

--- siteController.js
+++ siteController.js
@@ -5,12 +5,20 @@ $(document).ready(function(){
 	// pause the animation
 	setTimeout(function() {
 	    $("#mainCont").addClass("pauseAnimation");
-	    alert("Animation Paused");
+	    if($(event.target).text() == "Home") {
+		$("#greeting h1").text("Home");
+	    }else if($(event.target).text() == "About") {
+		$("#greeting h1").text("About");
+	    }else if($(event.target).text() == "Resume") {
+		$("#greeting h1").text("Resume");
+	    }else if($(event.target).text() == "Blog") {
+		$("#greeting h1").text("Blog");
+	    }
 	    $("#mainCont").removeClass("pauseAnimation");
 	}, 500);
 	setTimeout(function() {
 	    $("#mainCont").removeClass("mainContAnim");
-	}, 2500); // This second parameter number is the duration of the
+	}, 1500); // This second parameter number is the duration of the
 	    // animation in milliseconds.
     });
 });
