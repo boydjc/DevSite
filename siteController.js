1c8c342bb0b769b4fbbab19a075c77af2d0d4158
Joshua Boyd <jaboydfamily@gmail.com>
2021-05-16 11:57:08 -0400

    added javascript pause functionality for content transition

--- siteController.js
+++ siteController.js
@@ -2,5 +2,15 @@ $(document).ready(function(){
     $("#navBar a" ).click(function(event){
 	event.preventDefault();
 	$("#mainCont").addClass("mainContAnim");
+	// pause the animation
+	setTimeout(function() {
+	    $("#mainCont").addClass("pauseAnimation");
+	    alert("Animation Paused");
+	    $("#mainCont").removeClass("pauseAnimation");
+	}, 500);
+	setTimeout(function() {
+	    $("#mainCont").removeClass("mainContAnim");
+	}, 2500); // This second parameter number is the duration of the
+	    // animation in milliseconds.
     });
 });
