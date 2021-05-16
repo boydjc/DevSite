ad86c12840b3ef8b9f2097c5eb70a8e8b6841a0d
Joshua Boyd <jaboydfamily@gmail.com>
2021-05-16 15:08:08 -0400

    got the site to dynamically remove and readd home content

--- siteController.js
+++ siteController.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+    var currentPage = "Home";
+
     $("#navBar a" ).click(function(event){
 	event.preventDefault();
 	$("#mainCont").addClass("mainContAnim");
@@ -6,15 +9,25 @@ $(document).ready(function(){
 	setTimeout(function() {
 	    $("#mainCont").addClass("pauseAnimation");
 	    if($(event.target).text() == "Home") {
+		removeContent();
 		changeToHome();
+		currentPage = "Home";
 	    }else if($(event.target).text() == "About") {
+		removeContent();
 		changeToAbout();
+		currentPage = "About";
 	    }else if($(event.target).text() == "Resume") {
+		removeContent();
 		changeToResume();
+		currentPage = "Resume";
 	    }else if($(event.target).text() == "Blog") {
+		removeContent();
 		changeToBlog();
+		currentPage = "Blog";
 	    }else if($(event.target).text() == "Contact") {
+		removeContent();
 		changeToContact();
+		currentPage = "Contact";
 	    }
 
 	    $("#mainCont").removeClass("pauseAnimation");
@@ -25,27 +38,43 @@ $(document).ready(function(){
 	    // animation in milliseconds.
     });
 
+    
+    // removes web elements depending on what page we are currently on
+    function removeContent() {
+	if(currentPage == "Home") {
+            $("#profilePicCont").remove();
+	    $("#greeting").remove();
+	    $("#disc").remove();
+	}
+    }
+
     // changes the page to the home content
     function changeToHome() {
-	$("#greeting h1").text("Home");
+	$("#greeting h1").text("Joshua Boyd");
+	// Create profile picture div
+	$("#mainCont").append(
+	    '<div id="profilePicCont"><img src="joshFancyNew.jpg" id="profilePic" alt="fancy.jpg" height=300 width=307></div>'
+	);
+	$("#mainCont").append(
+	    '<div id="greeting"><h1>Joshua Boyd</h1></div>'
+	);
+	$("#mainCont").append(
+	    '<div id="disc"><ul><li id="listOne">Software Developer</li><li id="listTwo">Programming Enthusiast</li><li id="listThree">Computer Nerd</li></ul></div>'
+	);
     }
 
     // changes the page to the about content
     function changeToAbout() {
-	$("#greeting h1").text("About");
     }
 
     // changes the page to the blog content
     function changeToBlog() {
-	$("#greeting h1").text("Blog");
     }
 
     function changeToResume() {
-	$("#greeting h1").text("Resume");
     }
 
     // changes the page to the contact content
     function changeToContact() {
-	$("#greeting h1").text("Contact");
     }
 });
